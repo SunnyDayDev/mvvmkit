854c9390d091ed0c741eee061aa091ecd5df382f
Aleksandr Tcikin <sunnyday.development@gmail.com>
2018-06-05 20:33:59 +0300

    TextView onKeyEvent.

--- library/src/main/java/me/sunnydaydev/mvvmkit/binding/TextViewBindingAdapters.kt
+++ library/src/main/java/me/sunnydaydev/mvvmkit/binding/TextViewBindingAdapters.kt
@@ -1,6 +1,8 @@
 package me.sunnydaydev.mvvmkit.binding
 
 import android.databinding.BindingAdapter
+import android.view.KeyEvent
+import android.view.View
 import android.widget.TextView
 
 /**
@@ -14,6 +16,24 @@ object TextViewBindingAdapters {
     @BindingAdapter("errorHint")
     fun bindErrorHint(view: TextView, error: String?) {
         view.error = error
+    }
+
+    @JvmStatic
+    @BindingAdapter("onKeyEvent")
+    fun bindOnKeyDown(view: TextView, listener: OnKeyListener?) {
+
+        val viewListener: View.OnKeyListener? = listener?.let {
+            View.OnKeyListener { _, keyCode, keyEvent ->
+                it.onKey(keyCode, keyEvent)
+            }
+        }
+
+        view.setOnKeyListener(viewListener)
+
+    }
+
+    interface OnKeyListener {
+        fun onKey(keyCode: Int, event: KeyEvent): Boolean
     }
 
 }
