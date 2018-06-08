385f8cae55b59d1aac63f2dd0f2420c7344a96f0
Aleksandr Tcikin <sunnyday.development@gmail.com>
2018-06-08 18:14:08 +0300

    Remove dagger dependency.

--- library/src/main/java/me/sunnydaydev/mvvmkit/viewModel/ViewModelFactories.kt
+++ library/src/main/java/me/sunnydaydev/mvvmkit/viewModel/ViewModelFactories.kt
@@ -2,31 +2,25 @@ package me.sunnydaydev.mvvmkit.viewModel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import dagger.MapKey
-import javax.inject.Inject
-import javax.inject.Provider
-import kotlin.reflect.KClass
 
 /**
  * Created by sunny on 31.05.2018.
  * mail: mail@sunnydaydev.me
  */
 
-@MapKey
-annotation class ViewModelKey(val value: KClass<out ViewModel>)
+abstract class MVVMViewModelFactory: ViewModelProvider.Factory {
 
-class InjectableViewModelFactory @Inject constructor(
-        private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
-) : ViewModelProvider.Factory {
+    protected abstract val creators: Map<Class<out ViewModel>, () -> ViewModel>
 
     override fun <T : ViewModel> create(klass: Class<T>): T {
 
         val creator = creators[klass] ?: creators.entries
-                .firstOrNull { klass.isAssignableFrom(it.key) } ?.value
+                .firstOrNull { klass.isAssignableFrom(it.key) }
+                ?.value
         ?: throw IllegalArgumentException("Unknown view model class: $klass")
 
         @Suppress("UNCHECKED_CAST")
-        return creator.get() as T
+        return creator() as T
 
     }
 
