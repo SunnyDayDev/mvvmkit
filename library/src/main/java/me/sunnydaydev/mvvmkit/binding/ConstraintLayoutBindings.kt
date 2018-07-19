338f0fa26680ffd4dfc11457fe932aaf274ff577
Aleksandr Tcikin <sunnyday.development@gmail.com>
2018-07-19 09:44:53 +0300

    ConstraintLayout dimensions ratio.

---
+++ library/src/main/java/me/sunnydaydev/mvvmkit/binding/ConstraintLayoutBindings.kt
@@ -0,0 +1,34 @@
+package me.sunnydaydev.mvvmkit.binding
+
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.databinding.BindingAdapter
+
+/**
+ * Created by Aleksandr Tcikin (SunnyDay.Dev) on 19.07.2018.
+ * mail: mail@sunnydaydev.me
+ */
+
+object ConstraintLayoutBindings {
+
+    @JvmStatic
+    @BindingAdapter("layout_constraintDimensionRatio")
+    fun bindLayoutConstraintDimensionRatio(view: View, ratio: Float)
+            = bindLayoutConstraintDimensionRatio(view, (ratio * 10000).toInt(), 10000)
+
+    @JvmStatic
+    @BindingAdapter(
+            "layout_constraintDimensionRatioWidth",
+            "layout_constraintDimensionRatioHeight"
+    )
+    fun bindLayoutConstraintDimensionRatio(view: View, width: Int, height: Int) {
+
+        val lp = view.layoutParams as? ConstraintLayout.LayoutParams ?: return
+
+        lp.dimensionRatio = "$width:$height"
+
+        view.requestLayout()
+
+    }
+
+}
